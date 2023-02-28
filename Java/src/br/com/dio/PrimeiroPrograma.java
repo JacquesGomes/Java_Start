9f6fc3a56af99dac5ad649976440d70fce296b2c
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-02-28 14:18:12 -0300

    Initial environment

---
+++ Java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+        //crtl + shift + f10 para iniciar programa
+        //depois de iniciado shift + f10 para executar
+        //alt + insert para manipular arquivos e inserir novos
+
+        //transformar bloco de código em anotação = seleciona + ctrl
+        //+shift + /
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println(("Hello World! ") + (a+b));*/
+    }
+}
